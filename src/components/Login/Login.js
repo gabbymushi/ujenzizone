70209a6d48a92620f437dc43a419abd5d14b988c
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-03-30 12:13:24 +0300

    add sign up to ujenzi zone

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -117,7 +117,7 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
                   <CardBody className="text-center">
                     <div>
-                      <h2>Sign up</h2>
+                      <h2>Sign up to Ujenzi Zone</h2>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua.</p>
                       <Link to="/register">
