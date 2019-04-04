a8a1a9c7b3dd7661ee40e40434af7ad0d411dffb
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-04 10:41:08 +0300

    refactor login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -31,6 +31,7 @@ class Login extends Component {
       //     user_name: '',
       //     password: ''
       // });
+      localStorage.clear();
       const token = response.data.token;
                 // const userType = response.data.user.user_type;
                 //const userInfo = response.data.user;
@@ -54,6 +55,7 @@ class Login extends Component {
                 this.setState({ redirectToReferrer: true })
     }).catch((error) => {
       //console.log(error.request);
+      localStorage.clear();
       console.log(`😱 Axios request failed: ${error}`);
     });
   }
@@ -70,7 +72,7 @@ class Login extends Component {
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="8">
+            <Col md="8" style={{"marginTop":"250px"}} className="d-block d-sm-none">
               <CardGroup>
                 <Card className="p-4">
                   <CardBody>
@@ -114,7 +116,65 @@ class Login extends Component {
                     </Form>
                   </CardBody>
                 </Card>
-                <Card className="text-white bg-primary py-5 " >
+                <Card className="text-white bg-primary py-5" >
+                  <CardBody className="text-center">
+                    <div>
+                      <h2>Sign up to Ujenzi Zone</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                      <Link to="/register">
+                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now</Button>
+                      </Link>
+                    </div>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+            <Col md="8" className="d-none d-xl-block d-lg-block d-md-block d-sm-block">
+              <CardGroup>
+                <Card className="p-4">
+                  <CardBody>
+                    <Form onSubmit={this.handleSubmit}>
+                      <h1>Login</h1>
+                      <p className="text-muted">Sign In to your account</p>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          onChange={this.handleUserName}
+                          type="text"
+                          placeholder="Username"
+                          autoComplete="username"
+                        />
+                      </InputGroup>
+                      <InputGroup className="mb-4">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-lock"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          onChange={this.handlePassword}
+                          type="password"
+                          placeholder="Password"
+                          autoComplete="current-password"
+                        />
+                      </InputGroup>
+                      <Row>
+                        <Col xs="6">
+                          <Button color="primary" className="px-4">Login</Button>
+                        </Col>
+                        <Col xs="6" className="text-right">
+                          <Button color="link" className="px-0">Forgot password?</Button>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </CardBody>
+                </Card>
+                <Card className="text-white bg-primary py-5" >
                   <CardBody className="text-center">
                     <div>
                       <h2>Sign up to Ujenzi Zone</h2>
