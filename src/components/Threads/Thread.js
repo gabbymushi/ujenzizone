ec8d309d34b5f1a7c36b3042af1f55b1454777d0
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-20 11:46:48 +0300

    define function to handle comment textarea

--- src/components/Threads/Thread.js
+++ src/components/Threads/Thread.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
-import {Button,Input, Card, CardBody, CardHeader, Carousel, CarouselCaption, CarouselControl, CarouselIndicators, CarouselItem, Col, Row } from 'reactstrap';
-import API from '../../utils/API';
+import React, { Component } from "react";
+import {
+  Button,
+  Input,
+  Card,
+  CardBody,
+  CardHeader,
+  Carousel,
+  CarouselCaption,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselItem,
+  Col,
+  Row
+} from "reactstrap";
+import API from "../../utils/API";
 const items = [
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1607923e7e2%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1607923e7e2%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9296875%22%20y%3D%22217.75625%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 1',
-    caption: 'Slide 1',
+    src:
+      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1607923e7e2%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1607923e7e2%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9296875%22%20y%3D%22217.75625%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    altText: "Slide 1",
+    caption: "Slide 1"
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 2',
-    caption: 'Slide 2',
+    src:
+      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    altText: "Slide 2",
+    caption: "Slide 2"
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 3',
-    caption: 'Slide 3',
-  },
+    src:
+      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+    altText: "Slide 3",
+    caption: "Slide 3"
+  }
 ];
 
 class Thread extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -44,34 +59,70 @@ class Thread extends Component {
 
   next() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
+    const nextIndex =
+      this.state.activeIndex === items.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
   }
 
   previous() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? items.length - 1
+        : this.state.activeIndex - 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
   }
 
   goToIndex(newIndex) {
     if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
-  }
-  componentDidMount() {
-
-  }
-  getComments() {
-
-    let uri = 'comments/';
-    API.get(uri).then(response => {
-      this.setState({
-        comments: response.data
-      });
-      console.log(this.state.forums);
-    }).catch((error) => {
-      console.log(error)
+    this.setState({
+      activeIndex: newIndex
     });
+  }
+
+    handleComment = (e) => {
+        this.setState({ description: e.target.value });
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let forum = {
+            forum_name: this.state.forum_name,
+            description: this.state.description
+        }
+        API.post('comments/', forum).then((response) => {
+            // console.log(response);
+            console.log('👉 Returned data:', response);
+            this.setState({
+                forum_name: '',
+                description: ''
+
+            });
+            this.getForums();
+        }).catch((error) => {
+            //console.log(error.request);
+            console.log(`😱 Axios request failed: ${error}`);
+        });
+
+    }
+  componentDidMount() {}
+  getComments() {
+    let uri = "comments/";
+    API.get(uri)
+      .then(response => {
+        this.setState({
+          comments: response.data
+        });
+        console.log(this.state.forums);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   render() {
     const { activeIndex } = this.state;
@@ -84,15 +135,18 @@ class Thread extends Component {
     //   );
     // });
 
-    const slides2 = items.map((item) => {
+    const slides2 = items.map(item => {
       return (
         <CarouselItem
           onExiting={this.onExiting}
           onExited={this.onExited}
           key={item.src}
         >
-          <img className="d-block w-100" src={item.src} alt={item.altText} />
-          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+          <img className="d-block w-100" src={item.src} alt={item.altText} />{" "}
+          <CarouselCaption
+            captionText={item.caption}
+            captionHeader={item.caption}
+          />{" "}
         </CarouselItem>
       );
     });
@@ -100,65 +154,86 @@ class Thread extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
+          {" "}
           {/* <Col xs="12" xl="6">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i><strong>Carousel</strong>
-                <div className="card-header-actions">
-                  <a href="https://reactstrap.github.io/components/carousel/" rel="noreferrer noopener" target="_blank" className="card-header-action">
-                    <small className="text-muted">docs</small>
-                  </a>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <Carousel activeIndex={activeIndex} next={this.next} previous={this.previous} ride="carousel">
-                  {slides}
-                </Carousel>
-              </CardBody>
-            </Card>
-          </Col> */}
+                    <Card>
+                      <CardHeader>
+                        <i className="fa fa-align-justify"></i><strong>Carousel</strong>
+                        <div className="card-header-actions">
+                          <a href="https://reactstrap.github.io/components/carousel/" rel="noreferrer noopener" target="_blank" className="card-header-action">
+                            <small className="text-muted">docs</small>
+                          </a>
+                        </div>
+                      </CardHeader>
+                      <CardBody>
+                        <Carousel activeIndex={activeIndex} next={this.next} previous={this.previous} ride="carousel">
+                          {slides}
+                        </Carousel>
+                      </CardBody>
+                    </Card>
+                  </Col> */}{" "}
           <Col xs="12" xl="8">
             <Card>
               <CardHeader>
-                <i className="fa fa-align-justify"></i>
-                <strong>Carousel</strong>
-              </CardHeader>
+                <i className="fa fa-align-justify"> </i>{" "}
+                <strong> Carousel </strong>{" "}
+              </CardHeader>{" "}
               <CardBody>
-                <Carousel activeIndex={activeIndex} next={this.next} previous={this.previous}>
-                  <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-                  {slides2}
-                  <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-                  <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                <Carousel
+                  activeIndex={activeIndex}
+                  next={this.next}
+                  previous={this.previous}
+                >
+                  <CarouselIndicators
+                    items={items}
+                    activeIndex={activeIndex}
+                    onClickHandler={this.goToIndex}
+                  />{" "}
+                  {slides2}{" "}
+                  <CarouselControl
+                    direction="prev"
+                    directionText="Previous"
+                    onClickHandler={this.previous}
+                  />{" "}
+                  <CarouselControl
+                    direction="next"
+                    directionText="Next"
+                    onClickHandler={this.next}
+                  />{" "}
                 </Carousel>
-
-                {this.state.comments.length > 0 ?
-                  <p>
-                    {this.state.comments[0].comment}
-                  </p> : <p>No comments yet</p>
-                }
-                   <Input
-              onChange={this.handleBody}
-              value={this.state.body}
-              type="textarea"
-              name="textarea-input"
-              id="textarea-input" rows="9"
-              placeholder="Write your comment..." />
-              <Button color="primary" onClick={this.handleSubmit}>Comment</Button>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+                {this.state.comments.length > 0 ? (
+                  <p> {this.state.comments[0].comment} </p>
+                ) : (
+                  <p>No comments yet</p>
+                )}{" "}
+                <Input
+                  onChange={this.handleComment}
+                  value={this.state.body}
+                  type="textarea"
+                  name="textarea-input"
+                  id="textarea-input"
+                  rows="9"
+                  placeholder="Write your comment..."
+                />
+                <Button color="primary" onClick={this.handleSubmit}>
+                  {" "}
+                  Comment{" "}
+                </Button>{" "}
+              </CardBody>{" "}
+            </Card>{" "}
+          </Col>{" "}
+        </Row>{" "}
         {/* <Row>
-          <Col xs="12" md="8">
-            <Input
-              onChange={this.handleBody}
-              value={this.state.body}
-              type="textarea"
-              name="textarea-input"
-              id="textarea-input" rows="9"
-              placeholder="Write your comment..." />
-          </Col>
-        </Row> */}
+                  <Col xs="12" md="8">
+                    <Input
+                      onChange={this.handleBody}
+                      value={this.state.body}
+                      type="textarea"
+                      name="textarea-input"
+                      id="textarea-input" rows="9"
+                      placeholder="Write your comment..." />
+                  </Col>
+                </Row> */}{" "}
       </div>
     );
   }
