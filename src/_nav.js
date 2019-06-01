1f347c3d28da8de53d9b301592cda182c93db986
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-01 20:13:21 +0300

    remove admin panel menu item from user

--- src/_nav.js
+++ src/_nav.js
@@ -9,15 +9,15 @@ export default {
       //   text: 'NEW',
       // },
     },
-    {
-      title: true,
-      name: 'Admin Pannel',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
+    // {
+    //   title: true,
+    //   name: 'Admin Pannel',
+    //   wrapper: {            // optional wrapper object
+    //     element: '',        // required valid HTML5 element tag
+    //     attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+    //   },
+    //   class: ''             // optional class names space delimited list for title item ex: "text-center"
+    // },
     // {
     //   name: 'Forums',
     //   url: '/forums',
