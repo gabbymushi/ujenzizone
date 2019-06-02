347bb2841f288ff0e67a45857890d43175023ae5
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-02 10:43:53 +0300

    format document

--- src/App.js
+++ src/App.js
@@ -39,17 +39,17 @@ const Page500 = Loadable({
 });
 function SystemRoutes() {
   // return (<Route path="/" name="Home" component={DefaultLayout} />)
-  if (localStorage.getItem('user_type')=== "user") {
-      return (
-          <Route path="/" name="Dashboard" component={DefaultLayout}/>
-      );
-  } else if (localStorage.getItem('user_type')=== "admin") {
-      return (
-          <Route path="/" name="Admin Dashboard"
-                 component={AdminDefaultLayout}/>
-      );
-  }else{
-      return (<Route path="/login" exact component={Login} />)
+  if (localStorage.getItem('user_type') === "user") {
+    return (
+      <Route path="/" name="Dashboard" component={DefaultLayout} />
+    );
+  } else if (localStorage.getItem('user_type') === "admin") {
+    return (
+      <Route path="/" name="Admin Dashboard"
+        component={AdminDefaultLayout} />
+    );
+  } else {
+    return (<Route path="/login" exact component={Login} />)
   }
 }
 class App extends Component {
@@ -57,13 +57,13 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-          <Switch>
-            <Route exact path="/login" name="Login Page" component={Login} />
-            <Route exact path="/register" name="Register Page" component={Register} />
-            {/* <Route exact path="/404" name="Page 404" component={Page404} />
+        <Switch>
+          <Route exact path="/login" name="Login Page" component={Login} />
+          <Route exact path="/register" name="Register Page" component={Register} />
+          {/* <Route exact path="/404" name="Page 404" component={Page404} />
             <Route exact path="/500" name="Page 500" component={Page500} /> */}
-            <ProtectedRoute path="/"  component={SystemRoutes} />
-          </Switch>
+          <ProtectedRoute path="/" component={SystemRoutes} />
+        </Switch>
       </HashRouter>
     );
   }
