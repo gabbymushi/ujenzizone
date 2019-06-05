69f8a080764dc566d4a256698d4a9538925d6bfa
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-05 22:13:34 +0300

    hide right toggle on mobile

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -74,7 +74,7 @@ class DefaultHeader extends Component {
           </AppHeaderDropdown>
         </Nav>
         <AppAsideToggler className="d-md-down-none" />
-        <AppAsideToggler className="d-lg-none" mobile />
+        {/* <AppAsideToggler className="d-lg-none" mobile /> */}
       </React.Fragment>
     );
   }
